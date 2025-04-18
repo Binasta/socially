6411b9cccc652863caddab33c90b8ea727c1c6e1
beaufils <115308022+Binasta@users.noreply.github.com>
2025-04-18 11:18:37 +0200

    Setup & Authentication

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import { Button } from "@/components/ui/button";
+import ModeToggle from "@/components/ModeToggle";
+
+export default function Home() {
+  return (
+   <div>
+    <SignedOut>
+      <SignInButton mode="modal">
+      <Button>Sign in</Button>
+      </SignInButton>
+    </SignedOut>
+
+    <SignedIn>
+      <UserButton />
+    </SignedIn>
+    <ModeToggle/>
+   </div>
+  );
+}

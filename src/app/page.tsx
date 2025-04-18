05fca4e328096c16f4a0887de7cb06ff5e8dc5f8
beaufils <115308022+Binasta@users.noreply.github.com>
2025-04-18 12:27:26 +0200

    Auth & DB Sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,17 +11,8 @@ import ModeToggle from "@/components/ModeToggle";
 
 export default function Home() {
   return (
-   <div>
-    <SignedOut>
-      <SignInButton mode="modal">
-      <Button>Sign in</Button>
-      </SignInButton>
-    </SignedOut>
-
-    <SignedIn>
-      <UserButton />
-    </SignedIn>
-    <ModeToggle/>
+   <div className="m-4">
+    <h1>home page</h1>
    </div>
   );
 }
